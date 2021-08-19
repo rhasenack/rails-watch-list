d824a457011380eea7902b1aa7a59910a9fcd328
Ricardo Hasenack <rhasenack@gmail.com>
2021-08-19 11:37:32 -0300

    rake OK

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  before_destroy :prevent_destroy
+
+  validates :title, :overview, presence: true, allow_blank: false
+  # validates :rating, numericality: {greater_tha_or_equal_to: 0, less_than_or_equal_to: 10}
+  validates :title, uniqueness: true
+
+  private
+
+  def prevent_destroy
+    if self.bookmarks.count > 0
+      self.errors[:base] << "You may not delete this child."
+      return false
+    end
+  end
+end
